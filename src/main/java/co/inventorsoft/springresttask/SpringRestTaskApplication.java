14335def1f0ee28afc10fa2adc053e63d3ac5075
Bohdan Shostak <bohdanshostak93@gmail.com>
2022-09-28 11:08:39 +0300

    Created basic structure of the project

---
+++ src/main/java/co/inventorsoft/springresttask/SpringRestTaskApplication.java
@@ -0,0 +1,13 @@
+package co.inventorsoft.springresttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestTaskApplication.class, args);
+    }
+
+}
